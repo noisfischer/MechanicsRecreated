7609eb7c177d99cc4786415e238d6b5b89a28e44
noisfischer <noisfischer@gmail.com>
2024-02-07 20:23:18 -0500

    Health Bar and Animation Working

--- Source/MechanicsRecreated/Private/Character/FlashlightEnemybase.cpp
+++ Source/MechanicsRecreated/Private/Character/FlashlightEnemybase.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Character/FlashlightEnemybase.h"
 
 // Sets default values
@@ -9,12 +8,23 @@ AFlashlightEnemybase::AFlashlightEnemybase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Add custom tag for FlashlightComponent to recognize
+	Tags.Add("enemy");
+	
+	// Initialize shield health widget component and attach to mesh
+	ShieldWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("ShieldWidget"));
+	ShieldWidget->SetupAttachment(this->GetMesh());
+	ShieldWidget->SetWidgetSpace(EWidgetSpace::World);
+
 }
 
 // Called when the game starts or when spawned
 void AFlashlightEnemybase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Assign specific User Widget to widget component
+	UserWidget = Cast<UUserWidget>(ShieldWidget->GetUserWidgetObject());
 	
 }
 
@@ -22,13 +32,54 @@ void AFlashlightEnemybase::BeginPlay()
 void AFlashlightEnemybase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
+
+void AFlashlightEnemybase::FlashlightDamage_Implementation(float Damage)
+{
+	currentShield = FMath::Clamp(currentShield - Damage, 0, maxShield);	// Clamp currentShield value to min 0
+
+	if (currentShield == 0)
+	{
+		Tags.Add("shieldDown");								// Stops flashlight component line trace hit
+		UserWidget->SetVisibility(ESlateVisibility::Hidden);	// Hides shield health widget
+
+		PlayAnimMontage(shieldBreakMontage, 1.0, "None");
+	}
+
+	if (UserWidget && UserWidget->Implements<UDamageInterface>() && currentShield > -1)
+	{
+		Execute_FlashlightDamage(UserWidget, currentShield);	// Lower shield health bar widget
+	}
+	
+}
+
+float AFlashlightEnemybase::PlayAnimMontage(UAnimMontage* AnimMontage, float InPlayRate, FName StartSectionName)
+{
+	float MontageLength = 0.0f;		// Default to 0, indicating montage did not play
+	
+	if(AnimMontage)
+	{
+		UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+		
+		if(AnimInstance)
+		{
+			MontageLength = AnimInstance->Montage_Play(AnimMontage, InPlayRate);
+			
+			if (MontageLength > 0.f && StartSectionName != NAME_None)
+			{
+				AnimInstance->Montage_JumpToSection(StartSectionName, AnimMontage);
+			}
+		}
+	}
+
+	return MontageLength;
+}
+
+
 void AFlashlightEnemybase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
+
 
--- Source/MechanicsRecreated/Public/Character/FlashlightEnemybase.h
+++ Source/MechanicsRecreated/Public/Character/FlashlightEnemybase.h
@@ -3,18 +3,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
 #include "GameFramework/Character.h"
+#include "Interfaces/DamageInterface.h"
 #include "FlashlightEnemybase.generated.h"
 
 UCLASS()
-class MECHANICSRECREATED_API AFlashlightEnemybase : public ACharacter
+class MECHANICSRECREATED_API AFlashlightEnemybase : public ACharacter, public IDamageInterface
 {
 	GENERATED_BODY()
 
+	// Declare widget component for shield health
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+	UWidgetComponent* ShieldWidget;
+	
 public:
 	// Sets default values for this character's properties
 	AFlashlightEnemybase();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="1 - HEALTH")
+	float maxShield = 100;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="1 - HEALTH")
+	float currentShield = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="1 - HEALTH")
+	UAnimMontage* shieldBreakMontage;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="1 - HEALTH")
+	UUserWidget* UserWidget;
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -25,5 +44,9 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void FlashlightDamage_Implementation(float Damage) override;
+
+	virtual float PlayAnimMontage(UAnimMontage* AnimMontage, float InPlayRate, FName StartSectionName) override;
 
 };
